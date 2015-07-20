9b3d90b0c5fa93c7ab2c3de598f696f209c788e2
Jing <122300890@qq.com>
2015-07-21 00:13:49 +0800

    增加物理引擎

--- codes/xiu_pin_pile_box/bin-debug/lib/egret_file_list_native.js
+++ codes/xiu_pin_pile_box/bin-debug/lib/egret_file_list_native.js
@@ -121,5 +121,6 @@ var egret_file_list = [
 	"core/extension/resource/analyzer/FontAnalyzer.js",
 	"core/extension/resource/analyzer/SoundAnalyzer.js",
 	"core/extension/resource/analyzer/XMLAnalyzer.js",
-	"core/extension/resource/Resource.js"
+	"core/extension/resource/Resource.js",
+	"physics/p2.min.js"
 ];
